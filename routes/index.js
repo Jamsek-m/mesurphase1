e87df870699dedee95dd5d80a5486aaad1ed13ed
Jamsek-m <miha_jamsek@windowslive.com>
2016-11-02 14:12:18 +0100

    v1.0

---
+++ routes/index.js
@@ -0,0 +1,54 @@
+﻿var express = require('express');
+var router = express.Router();
+var config = require('../config.json');
+var mysql = require('mysql');
+var formidable = require('formidable');
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'uporabnik',
+	password : 'uporabnik',
+	database : 'mesurvey'
+});
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+	var currentDate = new Date();
+	currentDate.getTime();
+	var surveyEnd = Date.parse(config.surveyEnd);
+	
+	if (currentDate > surveyEnd) {
+		res.render('index', { isOver: true });
+	} else {
+		res.render('index', { isOver: false });
+	}
+});
+
+router.post('/submit', function (req, res, next) {
+	var form = formidable.IncomingForm();
+	form.parse(req, function (err, polja) {
+		if (err) throw err;
+		
+		var char_name = polja.Char_name;
+		var sug_name = polja.Sug_name;
+		var link = polja.Sug_name;
+		var desc = polja.Sug_desc;
+		var goal = polja.Sug_goal;
+
+		var sql = "INSERT INTO phase_one(CHAR_NAME, SUG_NAME, SUG_LINK, SUG_GOALS, SUG_DESC) VALUES ('" +
+				char_name + "','" +
+				sug_name + "','" +
+				link + "','" +
+				goal + "','" +
+				desc + "')";
+		
+		connection.query(sql, function (err, rows) {
+			if(err) throw err;
+			res.render('finish', {greeting: char_name});
+		});
+	});
+});
+
+
+module.exports = router;
